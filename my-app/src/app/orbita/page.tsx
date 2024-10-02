6beb60255d764a9a6df2644d7306c78593d4261f
mirelapsr <mirelarodrigues05@gmail.com>
2024-10-02 20:16:54 -0300

    atualizacao orbita

--- my-app/src/app/orbita/page.tsx
+++ my-app/src/app/orbita/page.tsx
@@ -1,79 +1,16 @@
-"use client"
-import { useEffect, useState } from 'react';
-import Menu from '@/Components/Menu/Menu'
-
-interface ApodData {
-  title: string;
-  explanation: string;
-  url: string;
-  hdurl?: string;
-  media_type: string;
-}
+import Menu from "@/Components/Menu/Menu";
+import ManualCard from "@/Components/Cards/ManualCard";
 
 export default function Orbita() {
-    const [apod, setApod] = useState<ApodData | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-  
-    useEffect(() => {
-      const fetchApod = async () => {
-        try {
-          const response = await fetch('/api/nasa');
-          if (!response.ok) {
-            throw new Error('Failed to fetch APOD');
-          }
-          const data: ApodData = await response.json();
-          setApod(data);
-        } catch (error) {
-          setError((error as Error).message);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchApod();
-    }, []);
-  
-    if (loading) {
-      return <p className="text-center text-gray-500">Loading...</p>;
-    }
-  
-    if (error) {
-      return <p className="text-center text-red-500">Error: {error}</p>;
-    }
-  
-    return (
-      <div>
-        <Menu />
+  return (
+    <div>
+      <Menu />
+      <ManualCard 
+        imageUrl="/img/orbita.png"
+        title="Órbitas Planetárias e Instabilidade"
+        description="Velikovsky sugeriu que Vênus, Marte e outros corpos celestes poderiam ter passado perto da Terra, perturbando as órbitas planetárias de forma significativa. Isso implicava em uma instabilidade no sistema solar que não era compatível com os cálculos newtonianos de órbitas estáveis a longo prazo."
+        />
+    </div>
+  )
+}
 
-        <div className="container mx-auto p-6">
-        <h1 className="text-3xl font-bold mb-4">Órbitas Planetárias e Instabilidade</h1>
-        <p className="mb-8 text-lg">
-          Velikovsky sugeriu que Vênus, Marte e outros corpos celestes poderiam ter passado perto da Terra, 
-          perturbando as órbitas planetárias de forma significativa. Isso implicava em uma instabilidade 
-          no sistema solar que não era compatível com os cálculos newtonianos de órbitas estáveis a longo prazo.
-        </p>
-  
-        {apod && (
-          <div>
-            {apod.media_type === 'image' ? (
-              <img src={apod.url} alt="NASA Astronomy Picture of the Day" className="w-full h-auto mb-4 rounded-lg shadow-md" />
-            ) : apod.media_type === 'video' ? (
-              <div className="aspect-w-16 aspect-h-9 mb-4">
-                <iframe
-                  src={apod.url}
-                  title="NASA Astronomy Picture of the Day"
-                  className="w-full h-full rounded-lg shadow-md"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </div>
-            ) : (
-              <p className="text-gray-500">Unsupported media type.</p>
-            )}
-          </div>
-        )}
-      </div>
-      </div>
-    );
-  }
